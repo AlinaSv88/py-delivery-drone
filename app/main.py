9d619d5690f3c2e4fdcb37879ca35a18de9c17cf
AlinaSv88 <s.syvokorovska@gmail.com>
2026-03-14 11:56:21 +0200

    Added suggested changes

--- app/main.py
+++ app/main.py
@@ -59,10 +59,9 @@ class DeliveryDrone(FlyingRobot):
             self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
-        if isinstance(cargo, Cargo):
-            if self.current_load is None:
-                if cargo.weight <= self.max_load_weight:
-                    self.current_load = cargo
+        if self.current_load is None:
+            if cargo.weight <= self.max_load_weight:
+                self.current_load = cargo
 
     def unhook_load(self) -> None:
         self.current_load = None
